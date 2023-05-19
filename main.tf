290430089701297323cd7ab8e650bbc85a263fe2
samya-elmain <93472667+samya-elmain@users.noreply.github.com>
2023-05-19 23:26:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,6 @@ terraform {
 # Configure the Azure provider
 provider "azurerm" {
   features {}
-
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
 }
 
 # Generate a random integer to create a globally unique name
