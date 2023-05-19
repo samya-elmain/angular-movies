d595b8165f2804c7a476a6cdfaba996890fb785f
samya-elmain <93472667+samya-elmain@users.noreply.github.com>
2023-05-19 21:08:31 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "movies-${random_integer.ri.result}"
+  location = "eastus"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "movies-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+# Create the web app for containers
+resource "azurerm_app_service" "webapp" {
+  name                = "movies-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  site_config {
+    always_on        = true
+    linux_fx_version = "DOCKER|samya01/movies"
+  }
+}
