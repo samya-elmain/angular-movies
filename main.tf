db3e185d8b6ab38a017c0051ab206d5ad3820f08
samya-elmain <93472667+samya-elmain@users.noreply.github.com>
2023-05-19 22:54:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
+resource "azurerm_app_service_plan" "appserviceplan" {
   name                = "mymovies-asp-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
